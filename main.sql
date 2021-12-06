ddb8523990f949c0f673e949b493ef38948cd7d7
Mateus Nadal <nadalmateus@hotmail.com>
2021-12-05 21:05:11 -0300

    feat: added more tables

--- main.sql
+++ main.sql
@@ -38,3 +38,63 @@ CREATE TABLE [Career] (
 )
 GO
 
+CREATE TABLE [Category] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(80) NOT NULL,
+    [Url] NVARCHAR(1024) NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Order] INT NOT NULL,
+    [Featured] BIT NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+)
+GO
+
+
+
+CREATE TABLE [Course] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Tag] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Level] TINYINT NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NOT NULL,
+    [Active] BIT NOT NULL,
+    [Free] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [AuthorId] UNIQUEIDENTIFIER NULL,
+    [CategoryId] UNIQUEIDENTIFIER NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Course_Author_AuthorId] FOREIGN key ([AuthorId]) REFERENCES [Author] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN key ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE NO ACTION,
+)
+GO
+
+CREATE TABLE [CareerItem] (
+    [CareerId] UNIQUEIDENTIFIER NOT NULL,
+    [CourseId] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Order] TINYINT NOT NULL,
+    CONSTRAINT [PK_CareerItem] PRIMARY KEY ([CourseId], [CareerId]),
+    CONSTRAINT [FK_CareerItem_Career_CareerId] FOREIGN key ([CareerId]) REFERENCES [Career] ([Id]),
+    CONSTRAINT [FK_CareerItem_Course_CourseId] FOREIGN key ([CourseId]) REFERENCES [Course] ([Id])
+)
+GO
+
+CREATE TABLE [StudentCourse] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [CourseId] UNIQUEIDENTIFIER NULL,
+    [Progress] TINYINT NOT NULL,
+    [Favorite] BIT NOT NULL,
+    [StudentId] UNIQUEIDENTIFIER NULL,
+    [StartDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NULL,
+    CONSTRAINT [PK_StudentCourse] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_StudentCourse_Course_CourseId] FOREIGN key ([CourseId]) REFERENCES [Course] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_StudentCourse_Student_StudentId] FOREIGN key ([StudentId]) REFERENCES [Student] ([Id]) ON DELETE NO ACTION,
+)
+GO
